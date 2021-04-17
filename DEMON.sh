4e1c60b2acea818a46541a8d7a582043f71ccb05
mfmvip <80322856+mfmvip@users.noreply.github.com>
2021-04-18 00:18:37 +0300

    Update DEMON.sh

--- DEMON.sh
+++ DEMON.sh
@@ -3,7 +3,7 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 Version=`lsb_release -rs | cut -f1 -d"."`
 install() {
 echo -e "\033[0;36m"
-echo -e "~ Welcome To Source DEMON - اهلا بك في سورس طوكيو ~"
+echo -e "~ Welcome To Source DEMON - اهلا بك في سورس الشيطان ~"
 echo -e "  ~ { Setting Up Offices - جاري رفع المكاتب } ~"
 echo -e "\033[0m"
 sudo apt-get update -y
